f5ce4f8901477aeabfc69b6a7a3be5087cab67d0
yonixw <yonixw@gmail.com>
2018-05-04 18:15:49 +0300

    .

--- GavHourReport/Program.cs
+++ GavHourReport/Program.cs
@@ -15,7 +15,7 @@ namespace GavHourReport
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new Form1());
         }
     }
 }
