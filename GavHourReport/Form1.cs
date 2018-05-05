8f5f80feaaf823be145f9e7779820c7dd2409118
yonixw <yonixw@gmail.com>
2018-05-06 00:54:50 +0300

    try catch !

--- GavHourReport/Form1.cs
+++ GavHourReport/Form1.cs
@@ -22,38 +22,46 @@ namespace GavHourReport
 
         private void loadTemplateToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string inputDate = Microsoft.VisualBasic.Interaction.InputBox("Put Date: <MM>/<YY>");
-            if (inputDate != "")
+            try
             {
-                DateTime day;
-                int currentMonth = -1;
-                if ( DateTime.TryParse("01/" + inputDate, out day)  ){
-                    dgvData.Rows.Clear();
-
-                    currentMonth = day.Month;
-
-                    DataGridViewCellStyle style = new DataGridViewCellStyle();
-                    style.BackColor = Color.Salmon;
-                    while (day.Month == currentMonth)
+                string inputDate = Microsoft.VisualBasic.Interaction.InputBox("Put Date: <MM>/<YY>");
+                if (inputDate != "")
+                {
+                    DateTime day;
+                    int currentMonth = -1;
+                    if (DateTime.TryParse("01/" + inputDate, out day))
                     {
+                        dgvData.Rows.Clear();
+
+                        currentMonth = day.Month;
+
+                        DataGridViewCellStyle style = new DataGridViewCellStyle();
+                        style.BackColor = Color.Salmon;
+                        while (day.Month == currentMonth)
+                        {
                             dgvData.Rows.Add(new object[] {
                                 day.ToString(dateFormat),
                                 culture.DateTimeFormat.GetDayName(day.DayOfWeek),
                                 "00:00", false, false, false });
 
-                        if (day.DayOfWeek == DayOfWeek.Friday || day.DayOfWeek == DayOfWeek.Saturday)
-                        {
-                            dgvData.Rows[dgvData.Rows.Count - 1].DefaultCellStyle = style;
-                            dgvData.Rows[dgvData.Rows.Count - 1].Cells["cIgnore"].Value = true;
-                        }
+                            if (day.DayOfWeek == DayOfWeek.Friday || day.DayOfWeek == DayOfWeek.Saturday)
+                            {
+                                dgvData.Rows[dgvData.Rows.Count - 1].DefaultCellStyle = style;
+                                dgvData.Rows[dgvData.Rows.Count - 1].Cells["cIgnore"].Value = true;
+                            }
 
-                        day = day.AddDays(1);
+                            day = day.AddDays(1);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cant understand \"" + inputDate + "\". please use MM/YY");
                     }
                 }
-                else
-                {
-                    MessageBox.Show("Cant understand \"" + inputDate + "\". please use MM/YY");
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
             }
         }
 
@@ -65,27 +73,53 @@ namespace GavHourReport
             dlg.CheckFileExists = true;
             dlg.Multiselect = false;
 
-            if (dlg.ShowDialog() == DialogResult.OK)
+            try
             {
-                Dictionary<string, TimeSpan> importData = ExcelFlow.RPTImport.import(dlg.FileName);
-
-                foreach (DataGridViewRow row in dgvData.Rows)
+                if (dlg.ShowDialog() == DialogResult.OK)
                 {
-                    string dateKey = row.Cells[0].Value.ToString();
-                    if (importData.ContainsKey(dateKey))
+                    Dictionary<string, TimeSpan> importData = ExcelFlow.RPTImport.import(dlg.FileName);
+
+                    foreach (DataGridViewRow row in dgvData.Rows)
                     {
-                        TimeSpan totalTime = importData[dateKey];
-                        row.Cells["cTIME"].Value = string.Format("{0:00}:{1:00}",
-                           (int)totalTime.TotalHours,
-                                totalTime.Minutes); 
+                        string dateKey = row.Cells[0].Value.ToString();
+                        if (importData.ContainsKey(dateKey))
+                        {
+                            TimeSpan totalTime = importData[dateKey];
+                            row.Cells["cTIME"].Value = string.Format("{0:00}:{1:00}",
+                               (int)totalTime.TotalHours,
+                                    totalTime.Minutes);
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
             }
         }
 
         private void eXPORTToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            try
+            {
+                OpenFileDialog dlgOpen = new OpenFileDialog();
+                dlgOpen.Title = "Choose GAV excel TEMPLATE";
+                dlgOpen.Filter = "Excel|*.xls;*.xlsx";
+                dlgOpen.CheckFileExists = true;
+                dlgOpen.Multiselect = false;
 
+                SaveFileDialog dlgSave = new SaveFileDialog();
+                dlgSave.Title = "Save GAV EXPORT";
+                dlgSave.Filter = "Excel|*.xls;*.xlsx";
+                dlgSave.CheckFileExists = true;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
         }
     }
 }
